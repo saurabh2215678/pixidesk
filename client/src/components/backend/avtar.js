1f86aa722f030fd0d0c37cdef0d2ecbfcb04734b
saurabh <saurabh.k@ehostinguk.com>
2022-09-27 19:24:31 +0530

    changes in box

--- client/src/components/backend/avtar.js
+++ client/src/components/backend/avtar.js
@@ -4,19 +4,19 @@ import { IconButton, ButtonBase } from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
 import userIcon from '../../assets/svg/user-solid.svg';
 
-const Avtar = () => {
+const Avtar = ({onSelectFile}) => {
     return(
         <div className="avtar_box">
             <ButtonBase>
                 <img alt="profile" className="avtar_image" src="https://bookingmedtravel.com/img/userimage.png"/>
                 <label className="avtar_icon">
                     <ReactSVG src={userIcon} />
-                    <input hidden accept="image/*" type="file" />
+                    <input hidden onChange={onSelectFile} accept="image/jpeg, image/png" type="file" />
                 </label>
             </ButtonBase>
             <div className="camera-icon">
                 <IconButton color="secondary" variant="contained" aria-label="upload picture" component="label">
-                    <input hidden accept="image/*" type="file" />
+                    <input hidden onChange={onSelectFile} accept="image/jpeg, image/png" type="file" />
                     <PhotoCamera />
                 </IconButton>
             </div>
