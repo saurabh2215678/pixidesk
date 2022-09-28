6eb6335132d54e474a205ac233a463058b7eddab
saurabh <saurabh.k@ehostinguk.com>
2022-09-28 20:13:06 +0530

    changes

--- client/src/components/backend/avtar.js
+++ client/src/components/backend/avtar.js
@@ -1,26 +1,127 @@
-import React from "react";
+import React, {useState,  useEffect, useRef} from "react";
 import { ReactSVG } from 'react-svg';
-import { IconButton, ButtonBase } from '@mui/material';
+import { IconButton, ButtonBase, Slide, Dialog } from '@mui/material';
 import { PhotoCamera } from '@mui/icons-material';
 import userIcon from '../../assets/svg/user-solid.svg';
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
 
-const Avtar = ({onSelectFile}) => {
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+  });
+
+const Avtar = () => {
+
+    const [selectedProfilePic, setSelectedProfilePic] = useState();
+    const [preview, setPreview] = useState();
+    const [profilePicModelOpen, setProfilePicModelOpen] = useState(false);
+
+    const [cropData, setCropData] = useState("");
+    const [cropper, setCropper] = useState();
+    const imageInputRef = useRef();
+
+    const getCropData = () => {
+        if (typeof cropper !== "undefined") {
+        cropper.getCroppedCanvas().toBlob((blob)=>{
+            const objectUrl = URL.createObjectURL(blob)
+            setCropData(objectUrl);
+            handleProfilePicModelClose();
+        });
+        }
+      };
+
+
+    const handleProfilePicModelOpen = () => {
+        setProfilePicModelOpen(true);
+    };
+  
+    const handleProfilePicModelClose = () => {
+        imageInputRef.current.value="";
+        setProfilePicModelOpen(false);
+    };
+
+    useEffect(()=>{
+        if(!selectedProfilePic){
+            setPreview(undefined);
+            return;
+        }
+
+        const objectUrl = URL.createObjectURL(selectedProfilePic)
+        setPreview(objectUrl)
+        // free memory when ever this component is unmounted
+        return () => URL.revokeObjectURL(objectUrl)
+    },[selectedProfilePic]);
+
+    useEffect(()=>{
+        if(!preview){
+            // show toast for Image not selected
+            return;
+        }
+        handleProfilePicModelOpen();
+        // open modal with privew image
+    },[preview]);
+
+
+    const onSelectFile = e => {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedProfilePic(undefined)
+            return
+        }
+        // I've kept this example simple by using the first image instead of multiple
+        setSelectedProfilePic(e.target.files[0]);
+    }
+
     return(
+        <>
         <div className="avtar_box">
             <ButtonBase>
-                <img alt="profile" className="avtar_image" src="https://bookingmedtravel.com/img/userimage.png"/>
+                {cropData !== '' ?
+                <img alt="profile" className="avtar_image" src={cropData}/>
+                :
                 <label className="avtar_icon">
                     <ReactSVG src={userIcon} />
                     <input hidden onChange={onSelectFile} accept="image/jpeg, image/png" type="file" />
                 </label>
+                }
             </ButtonBase>
             <div className="camera-icon">
                 <IconButton color="secondary" variant="contained" aria-label="upload picture" component="label">
-                    <input hidden onChange={onSelectFile} accept="image/jpeg, image/png" type="file" />
+                    <input ref={imageInputRef} hidden onChange={onSelectFile} accept="image/jpeg, image/png" type="file" />
                     <PhotoCamera />
                 </IconButton>
             </div>
         </div>
+        <Dialog
+            open={profilePicModelOpen}
+            TransitionComponent={Transition}
+            keepMounted
+            onClose={handleProfilePicModelClose}
+            aria-describedby="alert-dialog-slide-description"
+        >
+        <div className="image_cropper" style={{minWidth: 500, minHeight: 500}}>
+            <div id="image-helper"></div>
+            <Cropper
+                style={{ height: 400, width: "100%" }}
+                zoomTo={0}
+                initialAspectRatio={1}
+                preview=".img-preview"
+                src={preview}
+                viewMode={1}
+                minCropBoxHeight={10}
+                minCropBoxWidth={10}
+                background={false}
+                responsive={true}
+                autoCropArea={1}
+                checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+                onInitialized={(instance) => {
+                    setCropper(instance);
+                }}
+                guides={true}
+                />
+            <button onClick={getCropData}>crop</button>
+        </div>
+        </Dialog>
+        </>
     );
 }
 export default Avtar;
--- client/src/pages/backend/signup.js
+++ client/src/pages/backend/signup.js
@@ -1,63 +1,16 @@
-import React, {useState,  useEffect } from "react";
+import React from "react";
 import { ReactSVG } from 'react-svg';
 import logo from '../../assets/svg/logo.svg';
 import background from '../../assets/images/admin-splash.jpg';
 import {VisibilityOff, Visibility, AccountCircle, Email, ContactPhone} from '@mui/icons-material';
-import {FormControl, InputLabel, Input, InputAdornment, IconButton, Button, Dialog, Slide } from '@mui/material';
+import {FormControl, InputLabel, Input, InputAdornment, IconButton, Button } from '@mui/material';
 import { Link } from "react-router-dom";
 import Avtar from "../../components/backend/avtar";
 
 
-const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
-  });
-
-
 
 const SignUp = () => {
-    const [selectedProfilePic, setSelectedProfilePic] = useState();
-    const [preview, setPreview] = useState();
-    const [profilePicModelOpen, setProfilePicModelOpen] = useState(false);
 
-
-    const handleProfilePicModelOpen = () => {
-        setProfilePicModelOpen(true);
-    };
-  
-    const handleProfilePicModelClose = () => {
-        setProfilePicModelOpen(false);
-    };
-
-    useEffect(()=>{
-        if(!selectedProfilePic){
-            setPreview(undefined);
-            return;
-        }
-
-        const objectUrl = URL.createObjectURL(selectedProfilePic)
-        setPreview(objectUrl)
-        // free memory when ever this component is unmounted
-        return () => URL.revokeObjectURL(objectUrl)
-    },[selectedProfilePic]);
-
-    useEffect(()=>{
-        if(!preview){
-            // show toast for Image not selected
-            return;
-        }
-        handleProfilePicModelOpen();
-        // open modal with privew image
-    },[preview]);
-
-
-    const onSelectFile = e => {
-        if (!e.target.files || e.target.files.length === 0) {
-            setSelectedProfilePic(undefined)
-            return
-        }
-        // I've kept this example simple by using the first image instead of multiple
-        setSelectedProfilePic(e.target.files[0]);
-    }
     const [values, setValues] = React.useState({
         email: '',
         password: '',
@@ -95,21 +48,8 @@ const SignUp = () => {
                     <h3 className="heading5">Welcome to pixidesk.</h3>
                     <p className="para-md3">Already have an account? <Link to="/admin/login" className="text-theme">Log In Now</Link></p>
                     <div className="profile_pic">
-                        <Avtar onSelectFile={onSelectFile}/>
-                        <Dialog
-                            open={profilePicModelOpen}
-                            TransitionComponent={Transition}
-                            keepMounted
-                            onClose={handleProfilePicModelClose}
-                            aria-describedby="alert-dialog-slide-description"
-                        >
-                        <div className="image_cropper" style={{minWidth: 500, minHeight: 500}}>
-                            <div id="image-helper"></div>
-                            {/* <CroppieBox/> */}
-                            {/* {selectedProfilePic &&  <Croppie url={preview}/>} */}
-                            {selectedProfilePic &&  <img src={preview}/>}
-                        </div>
-                        </Dialog>
+                        <Avtar/>
+                        
                     </div>
                     
                     <FormControl fullWidth variant="standard">
